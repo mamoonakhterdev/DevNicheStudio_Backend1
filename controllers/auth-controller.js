432d4ab78ae15c7f000dae800e1e4b690948af47
mamoonakhterdev <mamoon.akhter@buggcy.com>
2024-08-19 19:47:35 +0500

    user data payload

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -86,4 +86,16 @@ const login = async(req, res)=> {
 }
 
 
-module.exports = {home, register, login};
+// *---------------------
+// to send user data - User Logic
+// *---------------------
+const user = async(req, res)=>{
+    try {
+        const userData = req.user;
+        console.log(userData);
+        return res.status(200).json({msg: userData});
+    } catch (error) {
+        console.log(`Error from the user route ${error}`);
+    }
+}
+module.exports = {home, register, login, user};
--- router/auth-router.js
+++ router/auth-router.js
@@ -4,12 +4,12 @@ const authControllers = require('../controllers/auth-controller');
 const signUpSchema = require("../validators/auth-signUp-validator");
 const validate = require('../middlewares/validate-middleware');
 const loginSchema = require('../validators/auth-login-validator');
-
+const authMiddleware = require("../middlewares/auth-middleware");
 
 router.route("/").get(authControllers
     .home);
 
 router.route("/register").post( validate(signUpSchema), authControllers.register);
 router.route("/login").post(validate(loginSchema),authControllers.login);
-
+router.route("/user").get(authMiddleware, authControllers.user);
 module.exports = router;
