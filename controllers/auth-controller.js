85b63a25195ed90ca95e45170194bc7015f878fa
mamoonakhterdev <mamoon.akhter@buggcy.com>
2024-08-19 21:41:33 +0500

    update

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -92,8 +92,7 @@ const login = async(req, res)=> {
 const user = async(req, res)=>{
     try {
         const userData = req.user;
-        console.log(userData);
-        return res.status(200).json({msg: userData});
+        return res.status(200).json({userData});
     } catch (error) {
         console.log(`Error from the user route ${error}`);
     }
